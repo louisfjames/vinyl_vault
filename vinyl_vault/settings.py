712e3df0a9a4d55ea2116bc8583ece05d6f82d67
louisfjames <louisfjames@hotmail.co.uk>
2026-08-02 14:08:46 +0100

    chore: move secret key and debug flag to env.py file

--- vinyl_vault/settings.py
+++ vinyl_vault/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-67g$tr30_(#@)+59_io3zxlzc$8r%+q2g!^z8nl+olj3tffw^p'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
