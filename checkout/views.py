3bb79f8a06335c15f6618ff3d3b168b8e2a4c5ac
louisfjames <louisfjames@hotmail.co.uk>
2026-09-13 12:41:31 +0100

    feat: add checkout view and url.py file

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from bag.contexts import bag_contents
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your bag is currently empty")
+        return redirect(reverse('albums'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    order_form = OrderForm()
+
+    context = {
+        'order_form': order_form,
+        'total': total,
+    }
+    return render(request, 'checkout/checkout.html', context)
