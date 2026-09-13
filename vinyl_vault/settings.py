cbc03bd94bce204da469051bfbbba6e598915418
louisfjames <louisfjames@hotmail.co.uk>
2026-09-13 12:18:46 +0100

    chore: installing django crispy forms and crisy bootstrap

--- vinyl_vault/settings.py
+++ vinyl_vault/settings.py
@@ -46,11 +46,16 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_countries',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'core',
     'albums',
     'bag',
     'checkout',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
