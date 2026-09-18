f943f5174f5579aef1bb4a37e5543a4775c5a251
louisfjames <louisfjames@hotmail.co.uk>
2026-09-18 14:17:50 +0100

    chore: adjust Order model to include stripe field

--- checkout/models.py
+++ checkout/models.py
@@ -23,6 +23,7 @@ class Order(models.Model):
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    stripe_pid = models.CharField(max_length=254, null=True, blank=True, default='')
     original_bag = models.TextField(null=False, blank=False, default='')
 
     def _generate_order_number(self):
