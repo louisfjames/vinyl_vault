b0ec1c129cb4d3db2d0cc3ba1f2908363efe3f1e
louisfjames <louisfjames@hotmail.co.uk>
2026-09-19 11:05:21 +0100

    refactor: adjusting checkout success page following confirmation of checkout flow working

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -31,6 +31,7 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
+
 // Handle realtime validation errors on the card element - provided by Code Institute / Stripe
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
@@ -45,4 +46,35 @@ card.addEventListener('change', function (event) {
     } else {
         errorDiv.textContent = '';        
     }
+});
+
+
+// Handle form submit - provided by Code Institute / Stripe
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
